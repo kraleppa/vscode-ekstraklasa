de9bb02c7f2f8bb740e4b1107ce1946f9ff251ce
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-20 14:19:08 +0100

    Basic webview added

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Standing, ApiResponse } from "./types";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const endpoint = "https://ekstraklasa.szarbartosz.com";
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "vscode-ekstraklasa" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ekstraklasa" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ekstraklasa.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-ekstraklasa!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-ekstraklasa.helloWorld", () => {
+      const panel = vscode.window.createWebviewPanel(
+        "ekstraklasaTable",
+        "Ekstraklasa Table",
+        vscode.ViewColumn.One,
+        {}
+      );
+      showView(panel);
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function showView(panel: vscode.WebviewPanel) {
+  const response = await fetch(endpoint);
+  const parsedResponse = (await response.json()) as ApiResponse;
+  const standings = parsedResponse.standings;
+
+  panel.webview.html = getWebviewContent(standings);
+}
+
+function getWebviewContent(standings: Standing[]) {
+  return /*html*/ `
+  <!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ekstraklasa Table</title>
+    <style>
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+      td, th {
+        border: 1px solid;
+        text-align: left;
+        padding: 8px;
+      }
+    </style>
+	</head>
+	<body>
+    <table>
+
+      ${standings
+        .map(
+          (standing) => /*html*/ `
+        <tr>
+          <td>
+
+            <img src="${standing.logoUrl}" alt="${standing.teamName}" width="20" height="auto"/>
+            <p>${standing.position}</p>
+            <p>${standing.teamName}</p>
+          </td>
+
+          <td>${standing.teamPoints}</td>
+          <td>${standing.wins}</td>
+          <td>${standing.draws}</td>
+          <td>${standing.losses}</td>
+          <td>${standing.goalsAgainst}</td>
+          <td>${standing.goalsFor}</td>
+        </tr>
+      `
+        )
+        .join("")}
+	</body>
+	</html>
+  `;
+}
