6663661c12ff06e0948facaad7a1c5583b49923c
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-22 12:49:59 +0100

    Basic refactor

--- src/extension.ts
+++ src/extension.ts
@@ -33,52 +33,7 @@ function getWebviewContent(standings: Standing[]) {
 	  	<meta charset="UTF-8">
 	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  	<title>Ekstraklasa Table</title>
-      <style>
-        @media only screen and (min-width: 950px) {
-          .wrapper {
-            margin-left: 10%;
-            margin-right: 10%;
-            width: 80%;
-          }
-        } 
-        table {
-          border-collapse: collapse;
-          width: 100%;
-        }
-        td, th {
-          text-align: left;
-          padding: 8px;
-        }
-        tr:not(:first-child) {
-          border-top: 1px solid;
-        }
-        .team-info {
-          display: flex;
-          align-items: center;
-          gap: 8px;
-        }
-        .image-wrapper {
-          width: 28px;
-          height: 28px;
-          display: flex;
-          justify-content: center;
-          margin-right: 8px;
-        }
-        .img { 
-          width: auto;
-          height: auto;
-        }
-        .last-five{
-          display: flex;
-          align-items: center;
-          gap: 4px;
-        }
-        .dot {
-          height: 15px;
-          width: 15px;
-          border-radius: 50%;
-        }
-      </style>
+      ${styles()}
 	  </head>
 	  <body>
       <div class="wrapper">
@@ -95,45 +50,51 @@ function getWebviewContent(standings: Standing[]) {
             <th>Pts</th>
             <th>Last 5</th>
           </tr>
-          ${standings
-            .map(
-              (standing) => /*html*/ `
-            <tr>
-              <td class="team-info">
-                <div class="image-wrapper">
-                  <img src="${standing.logoUrl}" alt="${standing.teamName}"/>
-                </div>
-                <p>${standing.position}</p>
-                <p>${standing.teamName}</p>
-              </td>
-              <td>${standing.gamesPlayed}</td>
-              <td>${standing.wins}</td>
-              <td>${standing.draws}</td>
-              <td>${standing.losses}</td>
-              <td>${standing.goalsFor}</td>
-              <td>${standing.goalsAgainst}</td>
-              <td>${standing.goalsDifference}</td>
-              <td>${standing.teamPoints}</td>
-              <td>
-                <div class="last-five">
-                  ${standing.lastResults
-                    .map(
-                      (result) => /*html*/ `
-                      <div class="dot" style="background-color: #${resultToColor(
-                        result
-                      )}"></div>
-                    `
-                    )
-                    .join("")}
-                </div>
-              </td>
-            </tr>
-          `
-            )
-            .join("")}
+          ${parseStandings(standings)}
       </div>
 	  </body>
 	</html>
+  `;
+}
+
+function parseStandings(standings: Standing[]) {
+  return standings.map((standing) => standingComponent(standing)).join("");
+}
+
+function standingComponent(standing: Standing) {
+  return /*html*/ `            
+    <tr>
+      <td class="team-info">
+        <div class="image-wrapper">
+          <img src="${standing.logoUrl}" alt="${standing.teamName}"/>
+        </div>
+        <p>${standing.position}</p>
+        <p>${standing.teamName}</p>
+      </td>
+      <td>${standing.gamesPlayed}</td>
+      <td>${standing.wins}</td>
+      <td>${standing.draws}</td>
+      <td>${standing.losses}</td>
+      <td>${standing.goalsFor}</td>
+      <td>${standing.goalsAgainst}</td>
+      <td>${standing.goalsDifference}</td>
+      <td>${standing.teamPoints}</td>
+      <td>
+        <div class="last-five">
+          ${parseLastResults(standing.lastResults)}
+        </div>
+      </td>
+    </tr>
+  `;
+}
+
+function parseLastResults(lastResults: MATCH_RESULT[]) {
+  return lastResults.map((result) => dotComponent(result)).join("");
+}
+
+function dotComponent(result: MATCH_RESULT) {
+  return /*html*/ `
+    <div class="dot" style="background-color: #${resultToColor(result)}"></div>
   `;
 }
 
@@ -146,4 +107,55 @@ function resultToColor(result: MATCH_RESULT) {
     case MATCH_RESULT.WON:
       return "00E100";
   }
+}
+
+function styles() {
+  return /*html*/ `
+    <style>
+      @media only screen and (min-width: 950px) {
+        .wrapper {
+          margin-left: 10%;
+          margin-right: 10%;
+          width: 80%;
+        }
+      } 
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+      td, th {
+        text-align: left;
+        padding: 8px;
+      }
+      tr:not(:first-child) {
+        border-top: 1px solid;
+      }
+      .team-info {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+      }
+      .image-wrapper {
+        width: 28px;
+        height: 28px;
+        display: flex;
+        justify-content: center;
+        margin-right: 8px;
+      }
+      .img { 
+        width: auto;
+        height: auto;
+      }
+      .last-five{
+        display: flex;
+        align-items: center;
+        gap: 4px;
+      }
+      .dot {
+        height: 15px;
+        width: 15px;
+        border-radius: 50%;
+      }
+    </style>
+  `;
 }
