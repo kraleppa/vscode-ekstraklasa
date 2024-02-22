6fa3b6467fe93d88b17eb966ab38b24333e7d017
Krzysztof Nalepa <kraleppa@gmail.com>
2024-02-22 12:37:27 +0100

    Formatting

--- src/extension.ts
+++ src/extension.ts
@@ -29,102 +29,110 @@ function getWebviewContent(standings: Standing[]) {
   return /*html*/ `
   <!DOCTYPE html>
 	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Ekstraklasa Table</title>
-    <style>
-      table {
-        border-collapse: collapse;
-        width: 100%;
-      }
-      td, th {
-        text-align: left;
-        padding: 8px;
-      }
-      tr:not(:first-child) {
-        border-top: 1px solid;
-      }
-      .team-info {
-        display: flex;
-        align-items: center;
-        gap: 8px;
-      }
-      .image-wrapper {
-        width: 28px;
-        height: 28px;
-        display: flex;
-        justify-content: center;
-        margin-right: 8px;
-      }
-      .img { 
-        width: auto;
-        height: auto;
-      }
-      .last-five{
-        display: flex;
-        align-items: center;
-        gap: 4px;
-      }
-      .dot {
-        height: 15px;
-        width: 15px;
-        border-radius: 50%;
-      }
-    </style>
-	</head>
-	<body>
-    <table>
-      <tr>
-        <th>Club</th>
-        <th>MP</th>
-        <th>W</th>
-        <th>D</th>
-        <th>L</th>
-        <th>GF</th>
-        <th>GA</th>
-        <th>GD</th>
-        <th>Pts</th>
-        <th>Last 5</th>
-      </tr>
-
-      ${standings
-        .map(
-          (standing) => /*html*/ `
-        <tr>
-          <td class="team-info">
-            <div class="image-wrapper">
-              <img src="${standing.logoUrl}" alt="${standing.teamName}"/>
-            </div>
-            <p>${standing.position}</p>
-            <p>${standing.teamName}</p>
-          </td>
-          <td>${standing.gamesPlayed}</td>
-          <td>${standing.wins}</td>
-          <td>${standing.draws}</td>
-          <td>${standing.losses}</td>
-          <td>${standing.goalsFor}</td>
-          <td>${standing.goalsAgainst}</td>
-          <td>${standing.goalsDifference}</td>
-          <td>${standing.teamPoints}</td>
-          <td>
-            <div class="last-five">
-              ${standing.lastResults
-                .map(
-                  (result) => /*html*/ `
-                  <div class="dot" style="background-color: #${resultToColor(
-                    result
-                  )}"></div>
-                `
-                )
-                .join("")}
-            </div>
-          </td>
-        </tr>
-      `
-        )
-        .join("")}
-	</body>
+	  <head>
+	  	<meta charset="UTF-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  	<title>Ekstraklasa Table</title>
+      <style>
+        @media only screen and (min-width: 950px) {
+          .wrapper {
+            margin-left: 10%;
+            margin-right: 10%;
+            width: 80%;
+          }
+        } 
+        table {
+          border-collapse: collapse;
+          width: 100%;
+        }
+        td, th {
+          text-align: left;
+          padding: 8px;
+        }
+        tr:not(:first-child) {
+          border-top: 1px solid;
+        }
+        .team-info {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
+        .image-wrapper {
+          width: 28px;
+          height: 28px;
+          display: flex;
+          justify-content: center;
+          margin-right: 8px;
+        }
+        .img { 
+          width: auto;
+          height: auto;
+        }
+        .last-five{
+          display: flex;
+          align-items: center;
+          gap: 4px;
+        }
+        .dot {
+          height: 15px;
+          width: 15px;
+          border-radius: 50%;
+        }
+      </style>
+	  </head>
+	  <body>
+      <div class="wrapper">
+        <table>
+          <tr>
+            <th>Club</th>
+            <th>MP</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>GF</th>
+            <th>GA</th>
+            <th>GD</th>
+            <th>Pts</th>
+            <th>Last 5</th>
+          </tr>
+          ${standings
+            .map(
+              (standing) => /*html*/ `
+            <tr>
+              <td class="team-info">
+                <div class="image-wrapper">
+                  <img src="${standing.logoUrl}" alt="${standing.teamName}"/>
+                </div>
+                <p>${standing.position}</p>
+                <p>${standing.teamName}</p>
+              </td>
+              <td>${standing.gamesPlayed}</td>
+              <td>${standing.wins}</td>
+              <td>${standing.draws}</td>
+              <td>${standing.losses}</td>
+              <td>${standing.goalsFor}</td>
+              <td>${standing.goalsAgainst}</td>
+              <td>${standing.goalsDifference}</td>
+              <td>${standing.teamPoints}</td>
+              <td>
+                <div class="last-five">
+                  ${standing.lastResults
+                    .map(
+                      (result) => /*html*/ `
+                      <div class="dot" style="background-color: #${resultToColor(
+                        result
+                      )}"></div>
+                    `
+                    )
+                    .join("")}
+                </div>
+              </td>
+            </tr>
+          `
+            )
+            .join("")}
+      </div>
+	  </body>
 	</html>
   `;
 }
